f51b83e1316054fbc4daf99c24766f9c2915f847
DESKTOP-7R2BHQS\13771 <13771961871@163.com>
2022-08-30 13:03:43 +0800

    优化登录页面和样式

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,3 +1,56 @@
 <template>
-  <div></div>
+  <div class="login-container">
+    <el-form class="login-form">
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="username" name="username" type="text" />
+      </el-form-item>
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="passeword" name="passeword" />
+        <span class="show-pwd">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+      </el-form-item>
+      <el-button type="primary" style="width: 100%; margin-bottom: 30px"
+        >登录</el-button
+      >
+    </el-form>
+  </div>
 </template>
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+  }
+  ::v-deep .el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+  }
+}
+</style>
